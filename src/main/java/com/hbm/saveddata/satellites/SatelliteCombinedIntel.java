04d2375b5443af4d574fc9f1212db67c79dc69e6
projectx-xo <79348979+projectx-xo@users.noreply.github.com>
2026-09-04 14:47:50 -0400

    feat: add combined intel satellite shell

---
+++ src/main/java/com/hbm/saveddata/satellites/SatelliteCombinedIntel.java
@@ -0,0 +1,24 @@
+package com.hbm.saveddata.satellites;
+
+import com.hbm.items.ModItems;
+import com.hbm.items.special.ItemSatellite.EnumSatType;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ChatComponentTranslation;
+import net.minecraft.util.IChatComponent;
+import net.minecraft.world.World;
+
+public class SatelliteCombinedIntel extends SatelliteBase {
+
+	@Override
+	public String getType() {
+		return "COMBINED_INTEL";
+	}
+
+	@Override
+	public IChatComponent[] getInfo(World world) {
+		return new IChatComponent[] {
+				new ChatComponentTranslation(ModItems.satellite.getUnlocalizedName(new ItemStack(ModItems.satellite, 1, EnumSatType.COMBINED_INTEL.ordinal())) + ".name")
+		};
+	}
+}
