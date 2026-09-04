bdff891ecc5a1a5ec375da0e4c2263a9b33c2526
projectx-xo <79348979+projectx-xo@users.noreply.github.com>
2026-09-04 14:47:43 -0400

    feat: add subsurface intel satellite shell

---
+++ src/main/java/com/hbm/saveddata/satellites/SatelliteSubsurfaceIntel.java
@@ -0,0 +1,24 @@
+package com.hbm.saveddata.satellites;
+
+import com.hbm.items.ModItems;
+import com.hbm.items.special.ItemSatellite.EnumSatType;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ChatComponentTranslation;
+import net.minecraft.util.IChatComponent;
+import net.minecraft.world.World;
+
+public class SatelliteSubsurfaceIntel extends SatelliteBase {
+
+	@Override
+	public String getType() {
+		return "SUBSURFACE_INTEL";
+	}
+
+	@Override
+	public IChatComponent[] getInfo(World world) {
+		return new IChatComponent[] {
+				new ChatComponentTranslation(ModItems.satellite.getUnlocalizedName(new ItemStack(ModItems.satellite, 1, EnumSatType.SUBSURFACE_INTEL.ordinal())) + ".name")
+		};
+	}
+}
