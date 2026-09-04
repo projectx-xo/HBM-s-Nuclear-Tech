fbdbc13db3d74fde379aca1416848098a20334dc
projectx-xo <79348979+projectx-xo@users.noreply.github.com>
2026-09-04 14:48:57 -0400

    feat: add intel classifications

---
+++ src/main/java/com/hbm/saveddata/satellites/intel/IntelClassification.java
@@ -0,0 +1,15 @@
+package com.hbm.saveddata.satellites.intel;
+
+public enum IntelClassification {
+	NATURAL,
+	STRUCTURE,
+	REINFORCED_STRUCTURE,
+	MACHINERY,
+	POWER,
+	COMMUNICATIONS,
+	LAUNCH_INFRASTRUCTURE,
+	CAVITY,
+	TUNNEL,
+	BUNKER,
+	POSSIBLE_SILO
+}
