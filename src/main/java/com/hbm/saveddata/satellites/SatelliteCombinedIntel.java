23ea57faddeba4fd792399e7f2c31a56c955c56d
projectx-xo <79348979+projectx-xo@users.noreply.github.com>
2026-09-04 14:52:52 -0400

    feat: wire combined intel lifecycle

--- src/main/java/com/hbm/saveddata/satellites/SatelliteCombinedIntel.java
+++ src/main/java/com/hbm/saveddata/satellites/SatelliteCombinedIntel.java
@@ -2,17 +2,23 @@ package com.hbm.saveddata.satellites;
 
 import com.hbm.items.ModItems;
 import com.hbm.items.special.ItemSatellite.EnumSatType;
+import com.hbm.saveddata.satellites.intel.IntelScanMode;
 
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ChatComponentTranslation;
 import net.minecraft.util.IChatComponent;
 import net.minecraft.world.World;
 
-public class SatelliteCombinedIntel extends SatelliteBase {
+public class SatelliteCombinedIntel extends SatelliteIntelligenceBase {
 
 	@Override
 	public String getType() {
 		return "COMBINED_INTEL";
+	}
+
+	@Override
+	public IntelScanMode getScanMode() {
+		return IntelScanMode.COMBINED;
 	}
 
 	@Override
