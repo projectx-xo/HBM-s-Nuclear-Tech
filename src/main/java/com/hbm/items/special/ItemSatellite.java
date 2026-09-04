d570ca5e5b04eec5c2c231beef2924ed3cbf7f5e
projectx-xo <79348979+projectx-xo@users.noreply.github.com>
2026-09-04 14:47:22 -0400

    feat: add intelligence satellite item variants

--- src/main/java/com/hbm/items/special/ItemSatellite.java
+++ src/main/java/com/hbm/items/special/ItemSatellite.java
@@ -6,6 +6,9 @@ import com.hbm.items.ISatChip;
 import com.hbm.items.ItemEnumMulti;
 import com.hbm.util.i18n.I18nUtil;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumChatFormatting;
@@ -31,6 +34,28 @@ public class ItemSatellite extends ItemEnumMulti implements ISatChip {
 		SCIENCE,
 		SCIENCE_ASSEMBLER,
 		SCIENCE_SENSOR,
+		SURFACE_RECON,
+		SUBSURFACE_INTEL,
+		COMBINED_INTEL,
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void registerIcons(IIconRegister reg) {
+		super.registerIcons(reg);
+		// Temporary artwork reuse until dedicated intelligence satellite textures are added.
+		this.icons[EnumSatType.SURFACE_RECON.ordinal()] = reg.registerIcon(this.getIconString() + ".spy");
+		this.icons[EnumSatType.SUBSURFACE_INTEL.ordinal()] = reg.registerIcon(this.getIconString() + ".scanner");
+		this.icons[EnumSatType.COMBINED_INTEL.ordinal()] = reg.registerIcon(this.getIconString() + ".spy");
+	}
+
+	@Override
+	public String getItemStackDisplayName(ItemStack stack) {
+		EnumSatType type = EnumSatType.values()[Math.max(0, Math.min(stack.getItemDamage(), EnumSatType.values().length - 1))];
+		if(type == EnumSatType.SURFACE_RECON) return "Surface Reconnaissance Satellite";
+		if(type == EnumSatType.SUBSURFACE_INTEL) return "Subsurface Intelligence Satellite";
+		if(type == EnumSatType.COMBINED_INTEL) return "Combined Intelligence Satellite";
+		return super.getItemStackDisplayName(stack);
 	}
 
 	@Override
