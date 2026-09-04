c945d34afc672f5f12fc9142d483b1b5dfa0e5b3
projectx-xo <79348979+projectx-xo@users.noreply.github.com>
2026-09-04 14:49:17 -0400

    feat: add intel finding model

---
+++ src/main/java/com/hbm/saveddata/satellites/intel/IntelFinding.java
@@ -0,0 +1,57 @@
+package com.hbm.saveddata.satellites.intel;
+
+import net.minecraft.nbt.NBTTagCompound;
+
+public class IntelFinding {
+	public IntelClassification classification = IntelClassification.STRUCTURE;
+	public int minX;
+	public int minY;
+	public int minZ;
+	public int maxX;
+	public int maxY;
+	public int maxZ;
+	public float confidence;
+	public boolean reinforced;
+	public boolean machinery;
+	public boolean power;
+	public boolean launchInfrastructure;
+	public boolean communications;
+
+	public NBTTagCompound writeToNBT() {
+		NBTTagCompound nbt = new NBTTagCompound();
+		nbt.setString("classification", classification.name());
+		nbt.setInteger("minX", minX);
+		nbt.setInteger("minY", minY);
+		nbt.setInteger("minZ", minZ);
+		nbt.setInteger("maxX", maxX);
+		nbt.setInteger("maxY", maxY);
+		nbt.setInteger("maxZ", maxZ);
+		nbt.setFloat("confidence", Math.max(0F, Math.min(1F, confidence)));
+		nbt.setBoolean("reinforced", reinforced);
+		nbt.setBoolean("machinery", machinery);
+		nbt.setBoolean("power", power);
+		nbt.setBoolean("launch", launchInfrastructure);
+		nbt.setBoolean("communications", communications);
+		return nbt;
+	}
+
+	public static IntelFinding readFromNBT(NBTTagCompound nbt) {
+		IntelFinding finding = new IntelFinding();
+		try {
+			finding.classification = IntelClassification.valueOf(nbt.getString("classification"));
+		} catch(Exception ignored) { }
+		finding.minX = nbt.getInteger("minX");
+		finding.minY = nbt.getInteger("minY");
+		finding.minZ = nbt.getInteger("minZ");
+		finding.maxX = nbt.getInteger("maxX");
+		finding.maxY = nbt.getInteger("maxY");
+		finding.maxZ = nbt.getInteger("maxZ");
+		finding.confidence = Math.max(0F, Math.min(1F, nbt.getFloat("confidence")));
+		finding.reinforced = nbt.getBoolean("reinforced");
+		finding.machinery = nbt.getBoolean("machinery");
+		finding.power = nbt.getBoolean("power");
+		finding.launchInfrastructure = nbt.getBoolean("launch");
+		finding.communications = nbt.getBoolean("communications");
+		return finding;
+	}
+}
