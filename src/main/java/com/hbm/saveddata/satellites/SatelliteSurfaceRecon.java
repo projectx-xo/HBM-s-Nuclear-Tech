6616078276278267f097fdbe3d0836395f79eb94
projectx-xo <79348979+projectx-xo@users.noreply.github.com>
2026-09-04 15:04:28 -0400

    fix: use direct surface recon display text

--- src/main/java/com/hbm/saveddata/satellites/SatelliteSurfaceRecon.java
+++ src/main/java/com/hbm/saveddata/satellites/SatelliteSurfaceRecon.java
@@ -1,11 +1,8 @@
 package com.hbm.saveddata.satellites;
 
-import com.hbm.items.ModItems;
-import com.hbm.items.special.ItemSatellite.EnumSatType;
 import com.hbm.saveddata.satellites.intel.IntelScanMode;
 
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.ChatComponentTranslation;
+import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.IChatComponent;
 import net.minecraft.world.World;
 
@@ -23,8 +20,6 @@ public class SatelliteSurfaceRecon extends SatelliteIntelligenceBase {
 
 	@Override
 	public IChatComponent[] getInfo(World world) {
-		return new IChatComponent[] {
-				new ChatComponentTranslation(ModItems.satellite.getUnlocalizedName(new ItemStack(ModItems.satellite, 1, EnumSatType.SURFACE_RECON.ordinal())) + ".name")
-		};
+		return new IChatComponent[] { new ChatComponentText("Surface Reconnaissance Satellite") };
 	}
 }
